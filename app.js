aea5c3caad5c264a321decac67faab940f50394b
osbym <marosb79@yahoo.com>
2021-10-10 17:31:28 -0500

    updated node.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const profileDataArgs = process.argv.slice(2);
 const printProfileData = (profileDataArr) => {
     //This
     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -8,6 +9,9 @@ console.log('================');
 
 // Is the same as this. . .
 profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
 
 
 
